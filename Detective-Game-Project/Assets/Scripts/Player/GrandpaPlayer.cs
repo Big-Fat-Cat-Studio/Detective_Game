5e6f32d139994e7b8f2babf423ef84eb04eb159f
Ino van den Berg <0928787@hr.nl>
2019-10-29 12:45:00 +0100

    scripts for switching between the 2 characters

---
+++ Detective-Game-Project/Assets/Scripts/Player/GrandpaPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrandpaPlayer : MonoBehaviour
+{
+    public bool is_active_player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(is_active_player)
+        {
+            print("grandpa is active");
+        }
+    }
+}
