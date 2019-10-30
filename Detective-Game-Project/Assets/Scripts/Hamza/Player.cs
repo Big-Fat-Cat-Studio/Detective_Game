e59f6494e39d2a6c999b8a43dab45cc0abd02d5c
Maher <mahermx1@outlook.com>
2019-10-30 14:51:08 +0100

    jump and glide code added to player script

--- Detective-Game-Project/Assets/Scripts/Hamza/Player.cs
+++ Detective-Game-Project/Assets/Scripts/Hamza/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour
     public float speed = 2.0f;
     public float rotationSpeed = 100.0f;
 
+    public float jumpForce;
+    public float glide;
+
     void Update()
     {
 
@@ -15,6 +18,28 @@ public class Player : MonoBehaviour
 
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
+
+        if (gameObject.GetComponent<Rigidbody>().velocity.y == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                gameObject.GetComponent<Rigidbody>().AddForce(0,jumpForce,0);
+            }
+        }
+        else if (gameObject.GetComponent<Rigidbody>().velocity.y < 0)
+        {
+           if (Input.GetKey(KeyCode.Space))
+            {
+                gameObject.GetComponent<Rigidbody>().AddForce(Physics.gravity = new Vector3 (0, glide ,0));
+            } 
+
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(Physics.gravity = new Vector3 (0, -9.81f ,0));
+        } 
+
+        
 
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
