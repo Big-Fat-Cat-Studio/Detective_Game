2ebd0cd6393b6d54c77b52f309882264c241a3d2
Maher <mahermx1@outlook.com>
2019-11-13 14:06:48 +0100

    dig mechanic

---
+++ Detective-Game-Project/Assets/Scripts/DigItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigItem : MonoBehaviour
+{
+    public GameObject Item;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        
+        if (collision.gameObject.tag == "Kid")
+        {
+            
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                Debug.Log("ouch");
+                gameObject.SetActive(false);
+                Instantiate(Item, gameObject.transform.position, gameObject.transform.rotation);
+            }
+            
+        }
+    }
+}
