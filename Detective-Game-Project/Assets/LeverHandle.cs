338686f4741e930b408c227d3e93997ca55a1fcc
Hamza Tokuchi <h@mza.jp>
2019-11-14 14:32:13 +0100

    item 1

---
+++ Detective-Game-Project/Assets/LeverHandle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverHandle : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 StartAngle;
+    [SerializeField] private Vector3 TargetAngle;
+
+    [SerializeField] private Vector3 CurrentAngle;
+
+    // Rotation should be clamped between -30 and 30
+    [SerializeField] private GameObject RotationBasePivotPoint;
+    [SerializeField] private GameObject RotationBaseAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartAngle = new Vector3(-30, 0, 0);
+        TargetAngle = new Vector3(30, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentAngle = new Vector3(Mathf.LerpAngle(StartAngle.x, TargetAngle.x, Time.deltaTime * 1), 0, 0);
+        RotationBasePivotPoint.transform.eulerAngles = CurrentAngle;
+    }
+}
---
+++ Detective-Game-Project/Assets/PressurePlate.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+
+    [SerializeField] private Collider ObjectCollider;
+    [SerializeField] private Renderer ObjectRenderer;
+
+    [SerializeField] [Tooltip("Debug Size Collider")] private Vector3 ObjectSizeCollider;
+    [SerializeField] [Tooltip("Debug Size Renderer")] private Vector3 ObjectSizeRenderer;
+
+    private Vector3 ObjectPressedSize;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjectSizeCollider = ObjectCollider.bounds.size;
+        ObjectSizeRenderer = ObjectRenderer.bounds.size;
+
+        ObjectPressedSize = new Vector3(0, (ObjectSizeCollider.y/2), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnChildTriggerEnter(Collider c, GameObject g)
+    {
+        gameObject.transform.position -= ObjectPressedSize;
+
+        
+    }
+
+    public void OnChildTriggerExit(Collider c, GameObject g)
+    {
+        gameObject.transform.position += ObjectPressedSize;
+    }
+}
