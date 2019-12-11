71a723f52d901dc81ecbbd3ebdcb7ee16218104d
martinvelden <mwf.vdvelden@gmail.com>
2019-12-11 10:40:01 +0100

    Sprinkler UI update

--- Detective-Game-Project/Assets/Scripts/Player/HumanPlayer.cs
+++ Detective-Game-Project/Assets/Scripts/Player/HumanPlayer.cs
@@ -10,6 +10,9 @@ namespace Scripts
         public bool isClimbing = false;
         public float climbingSpeed = 2.0f;
 
+        [HideInInspector]
+        public bool isInPuzzle = false;
+
         Rigidbody body;
         public GameObject umbrella;
         public bool umbrellaActiveOnStart;
@@ -62,17 +65,30 @@ namespace Scripts
             {
                 if (moveCamera)
                 {
-                    MoveCamera();
-
-                    if (inputType == InputType.Keyboard || (inputType == InputType.Controller && cameraDirection.x > -0.6 && cameraDirection.x < 0.6))
+                    if (isInPuzzle)
                     {
                         moveCamera = false;
+                    }
+
+                    else
+                    {
+                        MoveCamera();
+
+                        if (inputType == InputType.Keyboard || (inputType == InputType.Controller && cameraDirection.x > -0.6 && cameraDirection.x < 0.6))
+                        {
+                            moveCamera = false;
+                        }
                     }
                 }
 
                 if (move)
                 {
-                    if (isClimbing)
+                    if (isInPuzzle)
+                    {
+                        moveDirection = new Vector3(0, 0, 0);
+                        move = true;
+                    }
+                    else if (isClimbing)
                     {
                         gameObject.GetComponent<Animator>().SetBool("jumping", false);
                         moveDirection = new Vector3(0.0f, direction.y, direction.y * 0.3f);
@@ -102,7 +118,7 @@ namespace Scripts
                         gameObject.GetComponent<Animator>().SetBool("pushing", false);
                         gameObject.GetComponent<Animator>().SetFloat("forward/backward", Mathf.Round(direction.y));
                         gameObject.GetComponent<Animator>().SetFloat("sidewalk", Mathf.Round(direction.x));
-                        
+
                         if (Mathf.Round(direction.x) != 0)
                         {
                             gameObject.GetComponent<Animator>().SetBool("walksideways", true);
@@ -138,7 +154,7 @@ namespace Scripts
             }
         }
 
-        protected void OnSpecial()
+        protected void OnSpecial1()
         {
 
         }
