c69d4e07fbc4fcf6217a8bf3a4c213393dcd5ad4
Djesco <djescovandenberg@gmail.com>
2019-12-11 14:57:06 +0100

    Script voot michael

---
+++ Detective-Game-Project/Assets/Scripts/Environment/DestroyAndSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class DestroyAndSpawn : InteractableObject
+    {
+        public GameObject neededItem;
+        public GameObject itemToSpawn;
+        public string afterInteractMessage;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            interactableType = InteractableType.Destroyable;
+        }
+
+        public void interact(ActivePlayer player, GameObject playerItem)
+        {
+            if (neededItem == null || ReferenceEquals(playerItem, neededItem) ||
+                playerItem.name.Substring(0, playerItem.name.Length - 7) == neededItem.name)
+            {
+                this.gameObject.SetActive(false);
+                Destroy(this.gameObject);
+                if(itemToSpawn != null)
+                {
+                    Instantiate(itemToSpawn, this.transform);
+                }
+            }
+            else
+            {
+                GameManager.Instance.showAfterInteractText(player, afterInteractMessage);
+            }
+        }
+    }
+}
