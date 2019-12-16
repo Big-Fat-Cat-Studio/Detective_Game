33d7f6a25fb51f299a8bf4249dd0eb4caea1af2a
Hamza Tokuchi <h@mza.jp>
2019-12-16 10:46:13 +0100

    dolly layer fix

--- Detective-Game-Project/Assets/Scripts/TriggerDetection.cs
+++ Detective-Game-Project/Assets/Scripts/TriggerDetection.cs
@@ -40,10 +40,6 @@ public class TriggerDetection : MonoBehaviour
 				CameraCachedP = true;
 			}
 
-			Debug.Log($"Entering : {GetInstanceID()}");
-
-			Debug.Log($"Colliding with : {other.name}");
-
 			Scripts.GameManager.Instance.CameraFollow = DollyHuman;
 
 
