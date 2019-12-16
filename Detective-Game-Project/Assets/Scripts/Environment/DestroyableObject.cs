f6b8f5c51f7b4c50bfc4f67d2a44cbd2f7de1b15
Hamza Tokuchi <h@mza.jp>
2019-12-16 13:55:23 +0100

    current

--- Detective-Game-Project/Assets/Scripts/Environment/DestroyableObject.cs
+++ Detective-Game-Project/Assets/Scripts/Environment/DestroyableObject.cs
@@ -10,7 +10,6 @@ namespace Scripts
         public GameObject neededItem;
         public string afterInteractMessage;
 
-        // Start is called before the first frame update
         void Start()
         {
             interactableType = InteractableType.Destroyable;
