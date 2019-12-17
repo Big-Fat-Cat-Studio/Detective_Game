3f572aac952e1a95a61a0a7e47cf9669daa95c67
Djesco <djescovandenberg@gmail.com>
2019-12-17 12:23:43 +0100

    started work on toxic sprinklers

---
+++ Detective-Game-Project/Assets/ToxicSprinkler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToxicSprinkler : MonoBehaviour
+{
+    public ParticleSystem water;
+
+    public void Disable()
+    {
+        this.gameObject.GetComponent<BoxCollider>().isTrigger = true;
+        water.Stop();
+    }
+}
---
+++ Detective-Game-Project/Assets/temppressureplate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temppressureplate : MonoBehaviour
+{
+    public GameObject[] toxicSprinklers;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Human" || other.gameObject.tag == "Animal")
+        {
+            print("getriggered");
+            foreach(GameObject sprinkler in toxicSprinklers)
+            {
+                sprinkler.GetComponent<ToxicSprinkler>().Disable();
+            }
+        }
+    }
+}
