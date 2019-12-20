4593f8fc7a5cfcbe7637b4ddd7f5ce79a82c5572
gregoriec <gregorie_anti@live.nl>
2019-12-20 15:31:35 +0100

    respawn if detected

--- Detective-Game-Project/Assets/Scripts/ViewDetection.cs
+++ Detective-Game-Project/Assets/Scripts/ViewDetection.cs
@@ -2,108 +2,120 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ViewDetection : MonoBehaviour
+namespace Scripts
 {
-    public bool detectOnlyHuman;
-
-    public Transform Human;
-    public bool humanDetected, humanInView;
-
-    public Transform Dog;
-    public bool dogDetected, dogInView;
-
-    public bool inView;
-    private bool disabled;
-
-    void OnEnable()
+    public class ViewDetection : MonoBehaviour
     {
-        if (!disabled) return;
+        public bool detectOnlyHuman;
 
-        Collider collider = transform.GetChild(0).transform.gameObject.GetComponent<MeshCollider>();
+        public Transform Human;
+        public bool humanDetected, humanInView;
 
-        humanDetected = humanInView = collider.bounds.Contains(Human.position);
-        dogDetected = dogInView = collider.bounds.Contains(Dog.position);
+        public Transform Dog;
+        public bool dogDetected, dogInView;
 
-        disabled = false;
-    }
+        public bool inView;
+        private bool disabled;
 
-    void OnDisable()
-    {
-        disabled = true;
-    }
-
-    public void inFov(Transform target, string tag)
-    {
-        RaycastHit hit;
-        float offset = (tag == "Human" ? 1.5f : 0.5f);
-        Vector3 direction = (target.position + Vector3.up * offset);
-
-        if(Physics.Linecast(transform.position, direction, out hit))
+        void OnEnable()
         {
-            if (tag == "Animal")
+            if (!disabled) return;
+
+            Collider collider = transform.GetChild(0).transform.gameObject.GetComponent<MeshCollider>();
+
+            humanDetected = humanInView = collider.bounds.Contains(Human.position);
+            dogDetected = dogInView = collider.bounds.Contains(Dog.position);
+
+            disabled = false;
+        }
+
+        void OnDisable()
+        {
+            disabled = true;
+        }
+
+        IEnumerator ExampleCoroutine(GameObject gameObject)
+        {
+            yield return new WaitForSeconds(0.5f);
+            gameObject.GetComponent<RespawnPlayer>().Respawn();
+            OnChildTriggerExit(gameObject.GetComponent<Collider>());
+        }
+
+        public void inFov(Transform target, string tag)
+        {
+            RaycastHit hit;
+            float offset = (tag == "Human" ? 1.5f : 0.5f);
+            Vector3 direction = (target.position + Vector3.up * offset);
+
+            if(Physics.Linecast(transform.position, direction, out hit))
             {
-                dogInView = (LayerMask.LayerToName(hit.transform.gameObject.layer) == "Animal");
+                if (tag == "Animal")
+                {
+                    dogInView = (LayerMask.LayerToName(hit.transform.gameObject.layer) == "Animal");
+                    StartCoroutine(ExampleCoroutine(hit.transform.gameObject));
+                }
+                else if (tag == "Human")
+                {
+                    humanInView = (LayerMask.LayerToName(hit.transform.gameObject.layer) == "Human");
+                    StartCoroutine(ExampleCoroutine(hit.transform.gameObject));
+                }
             }
-            else if (tag == "Human")
+        }
+
+        public void OnChildTriggerEnter(Collider other)
+        {
+            if (other.tag == "Animal" && detectOnlyHuman) return;
+
+            if (other.tag == "Human")
             {
-                humanInView = (LayerMask.LayerToName(hit.transform.gameObject.layer) == "Human");
+                humanDetected = true;
+            }
+            else if (other.tag == "Animal")
+            {
+                dogDetected = true;
             }
         }
-    }
 
-    public void OnChildTriggerEnter(Collider other)
-    {
-        if (other.tag == "Animal" && detectOnlyHuman) return;
+        public void OnChildTriggerExit(Collider other)
+        {
+            if (other.tag == "Animal" && detectOnlyHuman) return;
 
-        if (other.tag == "Human")
-        {
-            humanDetected = true;
+            if (other.tag == "Human")
+            {
+                humanDetected = humanInView = false;
+            }
+            else if (other.tag == "Animal")
+            {
+                dogDetected = dogInView = false;
+            }
         }
-        else if (other.tag == "Animal")
-        {
-            dogDetected = true;
-        }
-    }
 
-    public void OnChildTriggerExit(Collider other)
-    {
-        if (other.tag == "Animal" && detectOnlyHuman) return;
+        void FixedUpdate()
+        {
+            if (humanDetected)
+            {
+                inFov(Human, "Human");
+            }
+            
+            if (dogDetected)
+            {
+                inFov(Dog, "Animal");
+            }
+            handleAlarm();
+        }
 
-        if (other.tag == "Human")
+        void handleAlarm()
         {
-            humanDetected = humanInView = false;
+            if (dogInView || humanInView)
+            {
+                inView = true;
+                GetComponent<Light>().color = Color.red;
+            }
+            else
+            {
+                inView = false;
+                GetComponent<Light>().color = Color.white;
+            }
         }
-        else if (other.tag == "Animal")
-        {
-            dogDetected = dogInView = false;
-        }
-    }
-
-    void FixedUpdate()
-    {
-        if (humanDetected)
-        {
-            inFov(Human, "Human");
-        }
-        
-        if (dogDetected)
-        {
-            inFov(Dog, "Animal");
-        }
-        handleAlarm();
-    }
-
-    void handleAlarm()
-    {
-        if (dogInView || humanInView)
-        {
-            inView = true;
-            GetComponent<Light>().color = Color.red;
-        }
-        else
-        {
-            inView = false;
-            GetComponent<Light>().color = Color.white;
-        }
-    }
+    }   
 }
