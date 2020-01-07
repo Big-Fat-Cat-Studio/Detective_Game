6ca7461444584fd2b8ea9cbb171e6fc65c704f56
Djesco <djescovandenberg@gmail.com>
2020-01-07 11:00:59 +0100

    more achievement work

--- Detective-Game-Project/Assets/Scripts/SaveSystem/EndLevel.cs
+++ Detective-Game-Project/Assets/Scripts/SaveSystem/EndLevel.cs
@@ -11,6 +11,7 @@ namespace Scripts
         //Variables
         public VideoPlayer endCutscene;
         public string nextLevelName;
+        public string achievementName;
         private bool cutsceneHasStarted = false;
 
         //Unity functions
@@ -35,6 +36,7 @@ namespace Scripts
         {
             //SaveData new_save = new SaveData(recalculateCompletedLevels(GameManager.Instance.saveData.completedLevels), this.nextLevelName, false);
             //SaveSystem.SaveProgress(new_save);
+            AchievementsManager.Instance.UnlockAchievement(achievementName);
             GameManager.Instance.PlayerCamera.SetActive(false);
             GameManager.Instance.PlayerCameraP2.SetActive(false);
             GameManager.Instance.InteractTextPlayerOne.SetActive(false);
