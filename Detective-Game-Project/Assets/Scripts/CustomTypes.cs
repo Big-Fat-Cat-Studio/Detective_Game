63f17fb473826d08bc526b4b88cb9719cd4aa65d
Djesco <djescovandenberg@gmail.com>
2020-01-07 11:43:22 +0100

    updated save system and added lamp

--- Detective-Game-Project/Assets/Scripts/CustomTypes.cs
+++ Detective-Game-Project/Assets/Scripts/CustomTypes.cs
@@ -10,13 +10,17 @@ namespace Scripts
         public string[] completedLevels;
         public string levelName;
         public int levelIndex;
+        public int pissCounter;
+        public int poopCounter;
         public bool isDummySave;
         public bool isCheckpoint;
 
-        public SaveData(int levelIndex, string levelName, bool isDummySave)
+        public SaveData(int levelIndex, int pissCounter, int poopCounter, string levelName, bool isDummySave)
         {
             this.levelName = levelName;
             this.levelIndex = levelIndex;
+            this.pissCounter = pissCounter;
+            this.poopCounter = poopCounter;
             //this.completedLevels = completedLevels;
             this.isDummySave = isDummySave;
             //this.isCheckpoint = isCheckpoint;
