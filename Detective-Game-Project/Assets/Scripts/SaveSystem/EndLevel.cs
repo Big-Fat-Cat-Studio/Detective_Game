07cea95f8dc6e8caef9fe8f69d9de87678ab91e3
Djesco <djescovandenberg@gmail.com>
2020-01-07 16:53:57 +0100

    warehouse bugfixes and pickup particle

--- Detective-Game-Project/Assets/Scripts/SaveSystem/EndLevel.cs
+++ Detective-Game-Project/Assets/Scripts/SaveSystem/EndLevel.cs
@@ -36,7 +36,7 @@ namespace Scripts
         {
             //SaveData new_save = new SaveData(recalculateCompletedLevels(GameManager.Instance.saveData.completedLevels), this.nextLevelName, false);
             //SaveSystem.SaveProgress(new_save);
-            AchievementsManager.Instance.UnlockAchievement(achievementName);
+            //AchievementsManager.Instance.UnlockAchievement(achievementName);
             GameManager.Instance.PlayerCamera.SetActive(false);
             GameManager.Instance.PlayerCameraP2.SetActive(false);
             GameManager.Instance.InteractTextPlayerOne.SetActive(false);
