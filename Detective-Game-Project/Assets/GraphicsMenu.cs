e900472cad20886683bd2078312772a0cb049934
Hamza Tokuchi <h@mza.jp>
2020-01-09 18:26:47 +0900

    testing new input switch

---
+++ Detective-Game-Project/Assets/GraphicsMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class GraphicsMenu : MonoBehaviour
+{
+    public AudioMixer MasterMixer;
+
+    public void SetVolume(float v)
+    {
+        MasterMixer.SetFloat("Volume", v);
+    } 
+}
