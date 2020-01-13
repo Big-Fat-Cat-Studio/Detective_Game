2c9dea0dbbf1ecb8c7faa662e70941c60a5c9a99
Leon Chen <leonchen@live.nl>
2020-01-13 18:08:18 +0100

    maincontrollermanager now updates properly

--- Detective-Game-Project/Assets/Scripts/Menu/MenuControls.cs
+++ Detective-Game-Project/Assets/Scripts/Menu/MenuControls.cs
@@ -3,31 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MenuControls : MonoBehaviour
+namespace Scripts
 {
-    public GameObject Player1, Player2;
-    int P1Value, P2Value;
-
-    void OnEnable()
+    public class MenuControls : MonoBehaviour
     {
-        Player1.GetComponent<Dropdown>().Select();
-        P1Value = Player1.GetComponent<Dropdown>().value;
-        P2Value = Player2.GetComponent<Dropdown>().value;
-    }
+        public GameObject Player1, Player2;
+        int P1Value, P2Value;
 
-    public void NoTwoKeyboardsP1()
-    {
-        if (P1Value == 0 && P2Value == 1)
+        void OnEnable()
         {
-            Player2.GetComponent<Dropdown>().value = 0;
+            Player1.GetComponent<Dropdown>().Select();
+            P1Value = Player1.GetComponent<Dropdown>().value;
+            P2Value = Player2.GetComponent<Dropdown>().value;
         }
-    }
 
-    public void NoTwoKeyboardsP2()
-    {
-        if (P2Value == 0 && P1Value == 1)
+        public void NoTwoKeyboardsP1()
         {
-            Player1.GetComponent<Dropdown>().value = 0;
+            if (P1Value == 0 && P2Value == 1)
+            {
+                Player2.GetComponent<Dropdown>().value = 0;
+            }
+        }
+
+        public void NoTwoKeyboardsP2()
+        {
+            if (P2Value == 0 && P1Value == 1)
+            {
+                Player1.GetComponent<Dropdown>().value = 0;
+            }
+        }
+
+        public void setInputDeviceP1()
+        {
+            if(Player1.GetComponent<Dropdown>().value == 0)
+            {
+                MainControllerManager.Instance.setInputDeviceP1(InputType.Controller);
+            }
+            else
+            {
+                MainControllerManager.Instance.setInputDeviceP1(InputType.Keyboard);
+            }
+        }
+
+        public void setInputDeviceP2()
+        {
+            if (Player2.GetComponent<Dropdown>().value == 0)
+            {
+                MainControllerManager.Instance.setInputDeviceP2(InputType.Controller);
+            }
+            else
+            {
+                MainControllerManager.Instance.setInputDeviceP2(InputType.Keyboard);
+            }
         }
     }
 }
