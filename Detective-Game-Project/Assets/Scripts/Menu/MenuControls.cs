113f6821b17354f067c2c3fc4070fe5f118e6e0d
martinvelden <mwf.vdvelden@gmail.com>
2020-01-13 17:41:40 +0100

    Options Graphics / Controls

---
+++ Detective-Game-Project/Assets/Scripts/Menu/MenuControls.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuControls : MonoBehaviour
+{
+    public GameObject Player1, Player2;
+    int P1Value, P2Value;
+
+    void OnEnable()
+    {
+        Player1.GetComponent<Dropdown>().Select();
+        P1Value = Player1.GetComponent<Dropdown>().value;
+        P2Value = Player2.GetComponent<Dropdown>().value;
+    }
+
+    public void NoTwoKeyboardsP1()
+    {
+        if (P1Value == 0 && P2Value == 1)
+        {
+            Player2.GetComponent<Dropdown>().value = 0;
+        }
+    }
+
+    public void NoTwoKeyboardsP2()
+    {
+        if (P2Value == 0 && P1Value == 1)
+        {
+            Player1.GetComponent<Dropdown>().value = 0;
+        }
+    }
+}
--- Detective-Game-Project/Assets/Scripts/Menu/MenuOptions.cs
+++ Detective-Game-Project/Assets/Scripts/Menu/MenuOptions.cs
@@ -9,19 +9,42 @@ public class MenuOptions : MonoBehaviour
     //remove after added
     public GameObject Resume, Graphics, Sound, Controls, Language;
 
-    void Start()
+    // void Start()
+    // {
+    //     // //remove after added
+    //     Sound.GetComponent<Button>().interactable = false;
+    //     // Controls.GetComponent<Button>().interactable = false;
+    //     Language.GetComponent<Button>().interactable = false;
+    // }
+    public void MenuReturn()
     {
-        // //remove after added
+        Resume.GetComponent<Button>().Select();
+    }
+
+    public void MenuDisable()
+    {
+        Resume.GetComponent<Button>().interactable = false;
         Graphics.GetComponent<Button>().interactable = false;
         Sound.GetComponent<Button>().interactable = false;
         Controls.GetComponent<Button>().interactable = false;
         Language.GetComponent<Button>().interactable = false;
     }
 
+    public void MenuEnable()
+    {
+        Resume.GetComponent<Button>().interactable = true;
+        Graphics.GetComponent<Button>().interactable = true;
+        // Sound.GetComponent<Button>().interactable = true;
+        Controls.GetComponent<Button>().interactable = true;
+        // Language.GetComponent<Button>().interactable = true;
+    }
 
     void OnEnable()
     {
-        //remove after added
+        // //remove after added
+        Sound.GetComponent<Button>().interactable = false;
+        // Controls.GetComponent<Button>().interactable = false;
+        Language.GetComponent<Button>().interactable = false;
         Resume.GetComponent<Button>().Select();
     }
 }
