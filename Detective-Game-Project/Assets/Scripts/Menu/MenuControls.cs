c85a9deafbf44c2f724d7aa53e4a08b89425c048
martinvelden <mwf.vdvelden@gmail.com>
2020-01-14 17:22:31 +0100

    fix

--- Detective-Game-Project/Assets/Scripts/Menu/MenuControls.cs
+++ Detective-Game-Project/Assets/Scripts/Menu/MenuControls.cs
@@ -6,19 +6,21 @@ using TMPro;
 
 namespace Scripts
 {
-    public GameObject Player1, Player2;
-    int P1Value, P2Value;
-
-    // ADD FUNCTION FOR INVERTED CAMERA
-
-    void OnEnable()
+    public class MenuControls : MonoBehaviour
     {
-        Player1.SetActive(false);
-        Player1.SetActive(true);
-        Player1.GetComponent<TMP_Dropdown>().Select();
-        P1Value = Player1.GetComponent<TMP_Dropdown>().value;
-        P2Value = Player2.GetComponent<TMP_Dropdown>().value;
-    }
+        public GameObject Player1, Player2;
+        int P1Value, P2Value;
+
+        // ADD FUNCTION FOR INVERTED CAMERA
+
+        void OnEnable()
+        {
+            Player1.SetActive(false);
+            Player1.SetActive(true);
+            Player1.GetComponent<TMP_Dropdown>().Select();
+            P1Value = Player1.GetComponent<TMP_Dropdown>().value;
+            P2Value = Player2.GetComponent<TMP_Dropdown>().value;
+        }
 
         public void NoTwoKeyboardsP1()
         {
