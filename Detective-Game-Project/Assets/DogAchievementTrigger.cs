8b5c026f5df0d461e83c5e9e511eefb23f79197b
Djesco <djescovandenberg@gmail.com>
2020-01-14 11:57:24 +0100

    added dog achievement trigger

---
+++ Detective-Game-Project/Assets/DogAchievementTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class DogAchievementTrigger : MonoBehaviour
+    {
+        //Variables
+        public string achievementName;
+        //Unity functions
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Animal")
+            {
+                AchievementsManager.Instance.UnlockAchievement(achievementName);
+            }
+        }
+    }
+}
