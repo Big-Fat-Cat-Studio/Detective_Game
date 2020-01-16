9a0c34ef95560bf0b37845e5a520dd0e9925b77e
Djesco <djescovandenberg@gmail.com>
2020-01-16 15:52:08 +0100

    added intro scene to game
    actual video not pushed because large file size problem

--- Detective-Game-Project/Assets/Scripts/Tutorial/ShowTutorialOnStart.cs
+++ Detective-Game-Project/Assets/Scripts/Tutorial/ShowTutorialOnStart.cs
@@ -1,14 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 namespace Scripts
 {
     public class ShowTutorialOnStart : Tutorial
     {
+        public VideoPlayer intro;
+
         void Start()
         {
-            Pause();
+            GameManager.Instance.PlayerCamera.SetActive(false);
+            GameManager.Instance.PlayerCameraP2.SetActive(false);
+            GameManager.Instance.InteractTextP1.SetActive(false);
+            GameManager.Instance.InteractTextP2.SetActive(false);
+            GameManager.Instance.AfterInteractTextP1.SetActive(false);
+            GameManager.Instance.AfterInteractTextP2.SetActive(false);
+            GameManager.Instance.Human.SetActive(false);
+            GameManager.Instance.Animal.SetActive(false);
+            GameManager.Instance.CutsceneCamera.SetActive(true);
+            intro.Play();
+            StartCoroutine(VideoChecker());
+        }
+        private IEnumerator VideoChecker()
+        {
+            WaitForSeconds timer = new WaitForSeconds(1.0f);
+            for(; ; )
+            {
+                yield return timer;
+                if(!intro.isPlaying)
+                {
+                    intro.gameObject.SetActive(false);
+                    GameManager.Instance.CutsceneCamera.SetActive(false);
+                    GameManager.Instance.PlayerCamera.SetActive(true);
+                    GameManager.Instance.PlayerCameraP2.SetActive(true);
+                    GameManager.Instance.Human.SetActive(true);
+                    GameManager.Instance.Animal.SetActive(true);
+                    Pause();
+                    break;
+                }
+            }
         }
     }
 }
