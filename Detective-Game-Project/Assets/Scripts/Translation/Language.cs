68f7e5977b7d8828d5c0fd56aa7d350058f4040d
martinvelden <mwf.vdvelden@gmail.com>
2020-01-22 14:03:32 +0100

    plz plz plz work

---
+++ Detective-Game-Project/Assets/Scripts/Translation/Language.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Language : MonoBehaviour
+{
+    // MENU
+    [HideInInspector]
+    public string menuMainPlay, menuMainNewGame, menuMainOptions, menuMainCredits, menuMainExit;
+    [HideInInspector]
+    public string menuPlay1, menuPlay2, menuPlay3;
+    [HideInInspector]
+    public string menuOptionsGraphics, menuOptionsSound, menuOptionsControls, menuOptionsLanguage;
+    [HideInInspector]
+    public string menuGraphicsVeryLow, menuGraphicsLow, menuGraphicsMedium, menuGraphicsHigh, menuGraphicsVeryHigh, menuGraphicsUltra, menuGraphicsFullscreen;
+
+    // menuSound = "";
+    [HideInInspector]
+    public string menuControlsController, menuControlsKeyboard;
+
+    // menuLanguage = ""; //ALL LANGUAGES WRITTEN IN LANGUAGE OF ORIGIN
+    [HideInInspector]
+    public string menuConfirmNewGame, menuConfirmExit, menuConfirmInGame;
+    [HideInInspector]
+    public string menuCommonBack, menuCommonYes, menuCommonNo;
+
+    // MENUINGAME
+    [HideInInspector]
+    public string menuResume, menuRestart, menuMainMenu;
+
+
+    // YARD
+    [HideInInspector]
+    public string yardWrench, yardBin, yardSnacks, yardElevatorFix, yardElevatorButton, yardEndLevel;
+    [HideInInspector]
+    public string yardKeyPart, yardKeyDig, yardKeyHuman, yardKeyCombine;
+
+    [HideInInspector]
+    public string tutorialCommon;
+    [HideInInspector]
+    public string tutorialPush, tutorialPushHeader, tutorialPushMessageTop, tutorialPushMessageBot;
+    [HideInInspector]
+    public string tutorialNature, tutorialNatureHeader, tutorialNatureMessageTop, tutorialNatureMessageBot;
+    [HideInInspector]
+    public string tutorialSnacks, tutorialSnacksHeader, tutorialSnacksMessageTop, tutorialSnacksMessageBot;
+    [HideInInspector]
+    public string tutorialUmbrella, tutorialUmbrellaHeader, tutorialUmbrellaMessageTop, tutorialUmbrellaMessageBot;
+    [HideInInspector]
+    public string tutorialMoveMessageWalk, tutorialMoveMessageLook, tutorialMoveMessageJump;
+
+
+}
---
+++ Detective-Game-Project/Assets/Scripts/Translation/Languages/NL.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NL : Language
+{
+    void Start()
+    {
+        menuMainPlay = "Spelen";
+        menuMainNewGame = "Nieuw Spel";
+        menuMainOptions = "Opties";
+        menuMainCredits = "Credits";
+        menuMainExit = "Afsluiten";
+
+        menuPlay1 = "Grootvaders tuin";
+        menuPlay2 = "Grootvaders kantoor";
+        menuPlay3 = "De Loods";
+
+        menuOptionsGraphics = "Grafisch";
+        menuOptionsSound = "Geluid";
+        menuOptionsControls = "Besturing";
+        menuOptionsLanguage = "Taal";
+
+        menuGraphicsVeryLow = "";
+        menuGraphicsLow = "";
+        menuGraphicsMedium = "";
+        menuGraphicsHigh = "";
+        menuGraphicsVeryHigh = "";
+        menuGraphicsUltra = "";
+        menuGraphicsFullscreen = "Volledig scherm";
+
+        // menuSound = "";
+
+        menuControlsController = "Controller";
+        menuControlsKeyboard = "Toetsenbord";
+
+        menuConfirmNewGame = "Alle vrijgespeelde levels gaan verloren, weet je het zeker?";
+        menuConfirmExit = "Weet je zeker dat je wilt afsluiten?";
+        menuConfirmInGame = "Je level progressie gaat verloren, weet je het zeker?";
+
+
+        menuCommonBack = "Vorige";
+        menuCommonYes = "Ja";
+        menuCommonNo = "Nee";
+
+        menuResume = "Doorgaan";
+        menuRestart = "Herstarten";
+        menuMainMenu = "Hoofdmenu";
+
+        // YARD
+
+        tutorialCommon = "Druk op <sprite=0> / <sprite=1> om de uitleg te lezen";
+
+        tutorialPushHeader = "Push objects";
+        tutorialPushMessageTop = "Press x to push an object with Kika";
+        tutorialPushMessageBot = "For heavier objects you need Daigo to push as well!";
+
+        tutorialNatureHeader = "Nature's call";
+        tutorialNatureMessageTop = "Sometimes a dog has to take a leak with [button]";
+        tutorialNatureMessageBot = "And yes... [button] does the rest";
+
+        tutorialSnacksHeader = "Dog treats";
+        tutorialSnacksMessageTop = "Press [x] near a dispenser for a dog treat";
+        tutorialSnacksMessageBot = "These treats will make Daigo go faster!";
+
+        tutorialUmbrellaHeader = "Umbrella";
+        tutorialUmbrellaMessageTop = "Kika can open her umbrella with [button]";
+        tutorialUmbrellaMessageBot = "Daigo can reach new heights by bouncing off the umbrella!";
+
+        tutorialMoveMessageWalk = "Use [button] to walk around";
+        tutorialMoveMessageLook = "Use [button] to look around";
+        tutorialMoveMessageJump = "Press [button] to jump";
+
+
+        yardBin = "Druk op <sprite=0> / <sprite=1> om de doos te duwen";
+        yardSnacks = "Druk op <sprite=0> / <sprite=1> om een snack te eten";
+        yardWrench = "Druk op <sprite=0> / <sprite=1> om de moersleutel op te pakken";
+        yardElevatorFix = "Druk op <sprite=0> / <sprite=1> om de elektriciteit in te schakelen";
+        yardElevatorButton = "Druk op <sprite=0> / <sprite=1> om de lift aan te zetten";
+        yardEndLevel = "De deur zit op slot! Ik heb een sleutel nodig om deze te openen";
+
+        yardKeyPart = "Druk op <sprite=0> / <sprite=1> om een deel van een sleutel op te pakken";
+        yardKeyDig = "Druk op <sprite=0> / <sprite=1> om een deel van een sleutel op te graven";
+        yardKeyHuman = "Je pakt een deel van een sleutel op en stopt het in je broekzak";
+        yardKeyCombine = "Je combineert alle sleutel delen om er een volledige van te maken";
+
+
+    }
+
+}
