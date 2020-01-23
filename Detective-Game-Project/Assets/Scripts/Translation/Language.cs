c0e03b7966f9cf78886f9958bce0956f4b10ff56
martinvelden <mwf.vdvelden@gmail.com>
2020-01-23 13:17:18 +0100

    Translations 1.0

---
+++ Detective-Game-Project/Assets/Scripts/Translation/Language.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Language : MonoBehaviour
+{
+    // MENU
+    [HideInInspector]
+    public string menuMainPlay, menuMainNewGame, menuMainOptions, menuMainCredits, menuMainExit;
+    [HideInInspector]
+    public string menuPlay1, menuPlay2, menuPlay3;
+    [HideInInspector]
+    public string menuOptionsGraphics, menuOptionsSound, menuOptionsControls, menuOptionsLanguage;
+    [HideInInspector]
+    public string menuGraphicsVeryLow, menuGraphicsLow, menuGraphicsMedium, menuGraphicsHigh, menuGraphicsVeryHigh, menuGraphicsUltra, menuGraphicsFullscreen;
+
+    // menuSound = "";
+    [HideInInspector]
+    public string menuControlsController, menuControlsKeyboard;
+
+    // menuLanguage = ""; //ALL LANGUAGES WRITTEN IN LANGUAGE OF ORIGIN
+    [HideInInspector]
+    public string menuConfirmNewGame, menuConfirmExit, menuConfirmInGame;
+    [HideInInspector]
+    public string menuCommonBack, menuCommonYes, menuCommonNo;
+
+    // MENUINGAME
+    [HideInInspector]
+    public string menuResume, menuRestart, menuMainMenu;
+
+    //COMMON
+    [HideInInspector]
+    public string commonDoorLocked, commonDoorOpen, commonElevator, commonRope, commonBox;
+    [HideInInspector]
+    public string commonKeyPickup, commonKeyPart, commonKeyDig, commonKeyHuman, commonKeyCombine;
+
+    //TUTORIAL
+    [HideInInspector]
+    public string tutorialCommon;
+    [HideInInspector]
+    public string tutorialPush, tutorialPushHeader, tutorialPushMessageTop, tutorialPushMessageBot;
+    [HideInInspector]
+    public string tutorialNature, tutorialNatureHeader, tutorialNatureMessageTop, tutorialNatureMessageBot;
+    [HideInInspector]
+    public string tutorialSnacks, tutorialSnacksHeader, tutorialSnacksMessageTop, tutorialSnacksMessageBot;
+    [HideInInspector]
+    public string tutorialUmbrella, tutorialUmbrellaHeader, tutorialUmbrellaMessageTop, tutorialUmbrellaMessageBot;
+    [HideInInspector]
+    public string tutorialMoveMessageWalk, tutorialMoveMessageLook, tutorialMoveMessageJump;
+
+    // YARD
+    [HideInInspector]
+    public string yardWrench, yardSnacks, yardFuseUse, yardFuseFix, yardFuseFixed;
+
+    // CITY
+    [HideInInspector]
+    public string cityBoxBig, cityPoster;
+    [HideInInspector]
+    public string cityDoorHandleMiss, cityDoorHandleDig, cityDoorHandle;
+
+    // DOCKS
+
+    [HideInInspector]
+    public string docksToolBox, docksBook, docksVent, docksVentStuck;
+    [HideInInspector]
+    public string docksPipe, docksPipeRotate, docksPipeMove, docksPipeSelect;
+}
---
+++ Detective-Game-Project/Assets/Scripts/Translation/Languages/NL.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NL : Language
+{
+    void Start()
+    {
+        menuMainPlay = "Spelen";
+        menuMainNewGame = "Nieuw Spel";
+        menuMainOptions = "Opties";
+        menuMainCredits = "Credits";
+        menuMainExit = "Afsluiten";
+
+        menuPlay1 = "Grootvaders tuin";
+        menuPlay2 = "Grootvaders kantoor";
+        menuPlay3 = "De Loods";
+
+        menuOptionsGraphics = "Grafisch";
+        menuOptionsSound = "Geluid";
+        menuOptionsControls = "Besturing";
+        menuOptionsLanguage = "Taal";
+
+        menuGraphicsVeryLow = "";
+        menuGraphicsLow = "";
+        menuGraphicsMedium = "";
+        menuGraphicsHigh = "";
+        menuGraphicsVeryHigh = "";
+        menuGraphicsUltra = "";
+        menuGraphicsFullscreen = "Volledig scherm";
+
+        // menuSound = "";
+
+        menuControlsController = "Controller";
+        menuControlsKeyboard = "Toetsenbord";
+
+        menuConfirmNewGame = "Alle vrijgespeelde levels gaan verloren, weet je het zeker?";
+        menuConfirmExit = "Weet je zeker dat je wilt afsluiten?";
+        menuConfirmInGame = "Je level progressie gaat verloren, weet je het zeker?";
+
+        menuCommonBack = "Vorige";
+        menuCommonYes = "Ja";
+        menuCommonNo = "Nee";
+
+        menuResume = "Doorgaan";
+        menuRestart = "Herstarten";
+        menuMainMenu = "Hoofdmenu";
+
+        // COMMON
+
+        commonDoorLocked = "De deur zit op slot! Ik heb een sleutel nodig om deze te openen";
+        commonDoorOpen = "Druk op <sprite=0> / <sprite=1> om de deur te openen";
+        commonElevator = "Druk op <sprite=0> / <sprite=1> om de lift aan te zetten";
+        commonRope = "Druk op <sprite=0> / <sprite=1> om aan het touw te trekken";
+        commonBox = "Druk op <sprite=0> / <sprite=1> om de doos te duwen";
+
+        commonKeyPickup = "Druk op <sprite=0> / <sprite=1> om de sleutel op te pakken";
+        commonKeyPart = "Druk op <sprite=0> / <sprite=1> om een deel van een sleutel op te pakken";
+        commonKeyDig = "Druk op <sprite=0> / <sprite=1> om een deel van een sleutel op te graven";
+        commonKeyHuman = "Je pakt een deel van een sleutel op en stopt het in je broekzak";
+        commonKeyCombine = "Je combineert alle sleutel delen om er een volledige van te maken";
+
+        // TUTORIAL
+
+        tutorialCommon = "Druk op <sprite=0> / <sprite=1> om de uitleg te lezen";
+
+        tutorialPushHeader = "Push objects";
+        tutorialPushMessageTop = "Press x to push an object with Kika";
+        tutorialPushMessageBot = "For heavier objects you need Daigo to push as well!";
+
+        tutorialNatureHeader = "Nature's call";
+        tutorialNatureMessageTop = "Sometimes a dog has to take a leak with [button]";
+        tutorialNatureMessageBot = "And yes... [button] does the rest";
+
+        tutorialSnacksHeader = "Dog treats";
+        tutorialSnacksMessageTop = "Press [x] near a dispenser for a dog treat";
+        tutorialSnacksMessageBot = "These treats will make Daigo go faster!";
+
+        tutorialUmbrellaHeader = "Umbrella";
+        tutorialUmbrellaMessageTop = "Kika can open her umbrella with [button]";
+        tutorialUmbrellaMessageBot = "Daigo can reach new heights by bouncing off the umbrella!";
+
+        tutorialMoveMessageWalk = "Use [button] to walk around";
+        tutorialMoveMessageLook = "Use [button] to look around";
+        tutorialMoveMessageJump = "Press [button] to jump";
+
+        // YARD
+
+        yardSnacks = "Druk op <sprite=0> / <sprite=1> om een snack te eten";
+        yardWrench = "Druk op <sprite=0> / <sprite=1> om de moersleutel op te pakken";
+        yardFuseUse = "Druk op <sprite=0> / <sprite=1> om de elektriciteit in te schakelen";
+        yardFuseFix = "De schakelaar is kapot, misschien kan ik hem repareren met een moersleutel";
+        yardFuseFixed = "Tof, ik denk dat ik het gerepareerd heb! Misschien werkt de lift nu";
+
+        // CITY
+
+        cityBoxBig = "Druk op <sprite=0> / <sprite=1> om de grote doos te duwen";
+        cityPoster = "Druk op <sprite=0> / <sprite=1> om de poster te bekijken";
+        cityDoorHandleMiss = "De deur heeft geen hendel... Misschien is hij ergens anders";
+
+        cityDoorHandleDig = "Druk op <sprite=0> / <sprite=1> om de deurhendel op te graven";
+        cityDoorHandle = "Druk op <sprite=0> / <sprite=1> om de deurhendel op te pakken";
+
+        // DOCKS
+
+        docksToolBox = "Druk op <sprite=0> / <sprite=1> om de gereedschapskist op te pakken";
+        docksBook = "Druk op <sprite=0> / <sprite=1> om het boek te lezen";
+        docksVent = "Druk op <sprite=0> / <sprite=1> om het rooster te verwijderen";
+        docksVentStuck = "Het rooster zit vast! Ik heb gereedschap nodig om het te openen";
+
+        docksPipe = "Druk op <sprite=0> / <sprite=1> om de leidingen te repareren";
+        docksPipeRotate = "draai pijp    <sprite=15><sprite=16> / <sprite=8><sprite=9>";
+        docksPipeMove = "verplaats pijp    <sprite=12><sprite=21> / <sprite=7><sprite=4>";
+        docksPipeSelect = "selecteer pijp    <sprite=14><sprite=18> / <sprite=5><sprite=6>";
+    }
+
+}
